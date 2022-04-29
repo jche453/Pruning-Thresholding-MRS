47776cfe4a3f7eb4615e5313a0a8173d108059c8
jche453 <junyu.chen@emory.edu>
2022-04-28 21:25:09 -0400

    Update MRS_func.R

--- MRS_func.R
+++ MRS_func.R
@@ -100,3 +100,43 @@ GenMRS = function(beta, SS, Pthred, CoMeRegion, CoMeBack = T, weightSE = F){
   results = list(pvalueinfo = pvalueinfo, MRS = MRS_final)
   return(results)
 }
+
+GenMRS_mediation = function(beta, SS, Pthred, CoMeRegion, CoMeBack = F){
+  pvalueinfo = matrix(NA, length(Pthred),5)
+  colnames(pvalueinfo) = c("Pvalue", "Number of CpG sites", "Numeber of CpG sites after matching with CoMeRegion", " Number of CpG sites after pruning", "Numeber of CpG sites after matching with DNA methylation data")
+  for (i in Pthred){
+    pvalue = 5 * 10 ^ (-i)
+    pvalueinfo[i-1,1] =  pvalue
+    SS_sub = SS[SS$Pvalue.x < pvalue & SS$Pvalue.y < pvalue, ]
+    pvalueinfo[i-1,2] =  nrow(SS_sub)
+    
+    if (CoMeBack){
+      SS_final = merge(SS_sub, CoMeRegion, by.x = "Marker", by.y = "clustercpg")
+      SS_final = data.frame(SS_final)
+      pvalueinfo[i-1,3] =  nrow(SS_final)
+      SS_final = SS_final %>% 
+        group_by(CoMethylCluster) %>% 
+        slice(which.min(Pvalue))
+      pvalueinfo[i-1,4] =  nrow(SS_final)
+    }else{
+      SS_final = merge(SS_sub, CoMeRegion, by.x = "Marker", by.y = "clustercpg")
+      SS_final = data.frame(SS_final)
+    }
+    betas_final = data.frame(t(beta[,SS_final$Marker, drop = F]))
+    betas_final$Marker = rownames(betas_final)
+    mat_final = merge(betas_final, SS_final, by = "Marker")
+    pvalueinfo[i-1,5] =  nrow(mat_final)
+    if (nrow(mat_final) > 0){
+      MRS = data.frame(t(as.matrix(mat_final[,2:(ncol(betas_final))])) %*% (mat_final$BETA))
+      colnames(MRS) = c(paste0("P",pvalue))
+      MRS$ID = rownames(MRS)
+      if (i == Pthred[1]){
+        MRS_final = MRS
+      }else{
+        MRS_final = merge(MRS, MRS_final, by = "ID", all = T)
+      }
+    }
+  }
+  results = list(pvalueinfo = pvalueinfo, MRS = MRS_final)
+  return(results)
+}
